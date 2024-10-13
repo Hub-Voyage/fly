26d3082673c202a4757356fc7efeccc08c90e9b2
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2024-10-13 08:39:28 -0300

    chore: add project bootstrap

---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  const count = ref(0)
+</script>
+
+<template>
+  <button type="button" class="button" @click="count++">count {{ count }}</button>
+</template>
+
+<style scoped>
+  .button {
+    background-color: #F8DB1C;
+  }
+</style>
