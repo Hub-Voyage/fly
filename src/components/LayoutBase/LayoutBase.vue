24a3ec30a5dbb9314130f0c85f453ac7032330c2
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2024-10-15 22:40:29 -0300

    add layout base component

---
+++ src/components/LayoutBase/LayoutBase.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+  import Header from "../Header/Header.vue"
+</script>
+<template>
+  <div class="intro">
+    <Header />
+  </div>
+  <slot />
+</template>
+<style>
+  .intro {
+    padding-bottom: 4px;
+    background-color: var(--color-primary-light);
+  }
+</style>
